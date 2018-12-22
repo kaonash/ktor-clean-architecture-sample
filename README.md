fb8228a4cd5c555dfd35e8f98770368f1a9ea9dd
Ken Shimizu <shimiken.plus.one@gmail.com>
2018-12-22 21:48:48 +0900

    Modify README

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 ### Libraries
 - [Ktor](https://github.com/ktorio/ktor)
 - [Koin](https://github.com/InsertKoinIO/koin)
-- [Jackson]()
-- [JUnit 5](), [MockK](), [AssertJ]()
+- [Jackson](https://github.com/FasterXML/jackson)
+- [JUnit 5](https://junit.org/junit5/), [MockK](https://github.com/mockk/mockk), [AssertJ](http://joel-costigliola.github.io/assertj/)
 
 ### How to Run
+Run main function of `infrastructure/src/main/kotlin/module/Application.kt`
--- infrastructure/src/main/kotlin/Application.kt
+++ infrastructure/src/main/kotlin/Application.kt
@@ -1,6 +1,5 @@
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.SerializationFeature
-import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.application.log
@@ -14,6 +13,8 @@ import io.ktor.locations.KtorExperimentalLocationsAPI
 import io.ktor.locations.Locations
 import io.ktor.response.respond
 import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 import io.ktor.util.KtorExperimentalAPI
 import mapper.ObjectMapperBuilder
 import module.KoinModuleBuilder
@@ -22,32 +23,35 @@ import routes.root
 
 @KtorExperimentalAPI
 @KtorExperimentalLocationsAPI
-fun Application.main() {
-    installKoin(KoinModuleBuilder.modules())
+fun main() {
+    val server = embeddedServer(Netty, port = 8082) {
+        installKoin(KoinModuleBuilder.modules())
 
-    install(ContentNegotiation) {
-        jackson {
-            ObjectMapperBuilder.build(this)
-            configure(SerializationFeature.INDENT_OUTPUT, true)
-            configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        install(ContentNegotiation) {
+            jackson {
+                ObjectMapperBuilder.build(this)
+                configure(SerializationFeature.INDENT_OUTPUT, true)
+                configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            }
+        }
+        install(StatusPages) {
+            exception<Throwable> { cause ->
+                log.error(cause.message, cause)
+                call.respond(HttpStatusCode.InternalServerError)
+            }
+        }
+        install(Locations)
+        install(CORS) {
+            method(HttpMethod.Options)
+            method(HttpMethod.Put)
+            method(HttpMethod.Delete)
+            anyHost()
+        }
+
+        routing {
+            root()
         }
     }
-    install(StatusPages) {
-        exception<Throwable> { cause ->
-            log.error(cause.message, cause)
-            call.respond(HttpStatusCode.InternalServerError)
-        }
-    }
-    install(Locations)
-    install(CORS) {
-        method(HttpMethod.Options)
-        method(HttpMethod.Put)
-        method(HttpMethod.Delete)
-        anyHost()
-    }
-
-    routing {
-        root()
-    }
+    server.start(wait = true)
 }
 
